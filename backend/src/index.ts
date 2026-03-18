809ef53346d12d1dbc70816ef0501f3e6964f9e6
Lachlan Van <55044908+ShongMao@users.noreply.github.com>
2026-03-18 22:12:52 +1100

    LV/FEAT/Setup db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ import authRoutes from './routes/auth';
 dotenv.config();
 
 const app: Application = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(helmet());
