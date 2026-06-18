8fd6d3be967b1ba7a77875fb9e387c0fc7210b85
Shwan Amin <z5684089@ad.unsw.edu.au>
2026-06-18 19:11:41 +1000

    Finished the statcard component with new prop added

--- frontend/src/app/(dashboard)/page.tsx
+++ frontend/src/app/(dashboard)/page.tsx
@@ -1,9 +1,13 @@
+import StatCard from "@/components/StatCard";
+
 export default function HomePage() {
   return (
     <div>
       {/* UPPER BOX SECTION */}
-      <div>
-        {/* This is where the three stat boxes should go */}
+      <div className="flex justify-evenly gap-5">
+        <StatCard label={"OPEN TASKS"} value={"5/9"} colour={"#F4EFD3"} />
+        <StatCard label={"UPCOMING EVENTS"} value={"4"} colour={"#B1C9DC"} />
+        <StatCard label={"NEW ANNOUNCEMENTS"} value={"6"} colour={"#ED6672"} />
       </div>
 
       {/* BELOW SECTION */}
--- frontend/src/components/StatCard.tsx
+++ frontend/src/components/StatCard.tsx
@@ -1,20 +1,23 @@
 interface StatCardProps {
   label: string; // e.g. "OPEN TASKS"
   value: string; // e.g. "1/3" or "4"
+  colour: string; // e.g "#ED6672"
 }
 
 // TODO: Style this component to match the design
 // - white card with rounded corners and a subtle border
 // - small uppercase label in muted grey at the top
 // - large bold number/value below the label
-export default function StatCard({ label, value }: StatCardProps) {
+export default function StatCard({ label, value, colour }: StatCardProps) {
   return (
     // bg-white = white background, rounded-lg = rounded corners, border = subtle outline, p-5 = space inside 5pixels
-    <div className="bg-white rounded-xl border border-gray-200 p-5">
-      {/* TODO: style the label — think small, uppercase, muted grey */}
-      <p>{label}</p>
-      {/* TODO: style the value — think large and bold */}
-      <p>{value}</p>
+    <div className="bg-white rounded-xl border border-[#8A94A3] w-[50%] flex overflow-hidden">
+      <div className="w-2 shrink-0" style={{ backgroundColor: colour }}></div>
+
+      <div className="p-5">
+        <p className="font-medium font-mono text-base text-[#8A94A3]">{label}</p>
+        <p className="font-bold text-3xl tracking-widest">{value}</p>
+      </div>
     </div>
   );
 }
